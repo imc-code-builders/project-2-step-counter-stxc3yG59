4196b98f3a8ae096824b93361cb18e938e202a50
stacey <stac2008.c@gmail.com>
2026-02-17 18:12:24 -0600

    done

--- project_work.py
+++ project_work.py
@@ -17,8 +17,13 @@ def get_changes(magnitudes):
 
     changes = []
 
-    # TODO: For each pair, calculate: next_value - current_value
+    
+def get_changes(magnitudes):
+    changes= []
 
+ # TODO: For each pair, calculate: next_value - current_value 
+    for i in range(len(magnitudes)-1):
+        changes.append(magnitudes[i+1]- magnitudes[i])  
     return changes
 
 
@@ -37,7 +42,9 @@ def count_peaks(changes, threshold):
     step_count = 0
 
     # TODO: Count changes greater than or equal to threshold
-
+    for change in changes:
+        if change >= threshold:
+            step_count += 1 
     return step_count
 
 
@@ -51,10 +58,11 @@ def count_steps(magnitudes, threshold=STEP_THRESHOLD):
     Returns:
         The number of steps the user took.
     """
-
+ 
     # TODO: Use get_changes() and count_peaks()
-
-    return 0
+    changes=get_changes(magnitudes)
+    steps=count_peaks(changes,threshold)
+    return steps
 
 
 if __name__ == "__main__":
